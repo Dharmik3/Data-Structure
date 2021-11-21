657f0da31113fdea63c7ec04fc96901aed351fae
Dharmik Patel <dharmikpatel.pepalla@gmail.com>
2021-11-21 18:26:09 +0530

    recursion section

---
+++ PRACTICAL-04/recursion-max-sum.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#include<conio.h>
+#define n 5
+void main()
+{
+
+    int arr[n],i;
+    printf("Enter 10 numbers :");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+
+    printf("Maximu num in the array is %d\n",maximum(arr,0));
+    printf("Sum of the array is %d",sum(arr,0));
+}
+
+int maximum(int arr[],int i)
+{
+    static int max=-1;
+    if(i==n)
+    return max;
+    else{
+    if(arr[i]>max)
+        max=arr[i];
+    maximum(arr,++i);
+    }
+    return max;
+}
+
+int sum(int arr[],int i)
+{
+    static int s=0;
+    if(i==n)
+        return s;
+    else{
+    s=s+arr[i];
+    sum(arr,++i);
+    }
+    return s;
+}
